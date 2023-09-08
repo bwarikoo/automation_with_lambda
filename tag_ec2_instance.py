662891f22be32a355ba84549add1c64c07ad426f
bakul warikoo <bwarikoo@gmail.com>
2023-09-07 23:46:42 -0700

    lambda function to tag newly created EC2 Instance with Owner ID

---
+++ tag_ec2_instance.py
@@ -0,0 +1,19 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+   ec2 = boto3.client('ec2')
+   def lambda_handler(event, context):
+      instanceId = event['detail']['responseElements']['instancesSet']['items'][0]['instanceId']
+      userIdentity = event['detail']['userIdentity']['accountId']
+      ec2.create_tags(
+        Resources = [
+            instanceId
+        ],
+        Tags = [
+            {
+                'Key': 'OwnerId',
+                'Value': userIdentity
+           },    
+        ]
+      )
