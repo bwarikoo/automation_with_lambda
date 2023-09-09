8b532b875344d937f662e5a3c0c2d14c68b165b6
Bakul Warikoo <32089999+bwarikoo@users.noreply.github.com>
2023-09-09 13:12:22 -0700

    Update tag_ec2_instance.py

--- tag_ec2_instance.py
+++ tag_ec2_instance.py
@@ -1,9 +1,9 @@
 import json
 import boto3
 
+ec2 = boto3.client('ec2')
+
 def lambda_handler(event, context):
-   ec2 = boto3.client('ec2')
-   def lambda_handler(event, context):
       instanceId = event['detail']['responseElements']['instancesSet']['items'][0]['instanceId']
       userIdentity = event['detail']['userIdentity']['accountId']
       ec2.create_tags(
